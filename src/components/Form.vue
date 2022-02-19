f39e0288dd5975a1972042d465b8e4953b88c8ac
Clarisa <clarisa.ang95@gmail.com>
2022-02-19 15:00:34 +0000

    Validate form with vuelidate

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,15 +1,23 @@
 <template>
-  <form class="form" @submit.prevent="submitForm">
-    <form-item v-model="interest.fullName" label="Full Name" placeholder="Your full name"></form-item>
-    <form-item v-model="interest.movie" label="Favourite Movie" placeholder="Your favourite movie"></form-item>
-    <form-item v-model="interest.book" label="Favourite Book" placeholder="Your favourite book"></form-item>
-    <simple-button class="form__button" nativeType="submit">Submit</simple-button>
+  <form class="form" @submit.prevent="validateForm">
+    <!-- Full name -->
+    <form-item v-model="interest.fullName" label="Full Name" placeholder="Your full name"/>
+    <p class="form__error" v-if="submitted && !$v.interest.fullName.required">Full name is required</p>
+    <!-- Movie -->
+    <form-item v-model="interest.movie" label="Favourite Movie" placeholder="Your favourite movie" />
+    <p class="form__error" v-if="submitted && !$v.interest.movie.required">Movie is required</p>
+    <!-- Book -->
+    <form-item v-model="interest.book" label="Favourite Book" placeholder="Your favourite book" />
+    <p class="form__error" v-if="submitted && !$v.interest.book.required">Book is required</p>
+    <!-- Submit -->
+    <simple-button class="form__button" nativeType="submit">Update</simple-button>
   </form>
 </template>
 
 <script>
 import FormItem from '@/components/FormItem.vue';
 import SimpleButton from '@/components/SimpleButton.vue';
+import { required } from 'vuelidate/lib/validators';
 
 export default {
   data() {
@@ -19,13 +27,29 @@ export default {
         movie: '',
         book: '',
       },
+      submitted: false,
     };
+  },
+  validations: {
+    interest: {
+      fullName: { required },
+      movie: { required },
+      book: { required },
+    },
   },
   components: {
     FormItem,
     SimpleButton,
   },
   methods: {
+    validateForm() {
+      this.submitted = true;
+      // Vuelidate validation
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        this.submitForm();
+      }
+    },
     submitForm() {
       console.log('Interest has been updated');
     },
@@ -40,6 +64,11 @@ export default {
   align-items: center;
   &__button {
     margin-top: 15px;
+  }
+  &__error {
+    margin: 2px 0 6px 0;
+    font-weight: 500;
+    color: $red;
   }
 }
 </style>
